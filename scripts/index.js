f4321aa2159e3d3abf7042ee920285e00d788e24
izajasz13 <karoldd13@gmail.com>
2019-11-17 21:24:29 +0100

    Slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 import {attachSectionListeners} from './sectionListeners.js';
+import {Slider} from './sliderClass.js';
 
 attachSectionListeners();
+
+const newSlider = document.querySelector('.slider1');
+new Slider(newSlider, 3000, ['img/odnowa_ig-05.png', 'img/grupy domowe.png'], 'main')
+
+const newSlider2 = document.querySelector('.slider2');
+new Slider(newSlider2, 3000, ['img/odnowa_ig-05.png', 'img/grupy domowe.png'], 'grupy')
---
+++ scripts/sliderClass.js
@@ -0,0 +1,42 @@
+export class Slider{
+    constructor(slider, time, imgList, id){
+        this.slider = slider;
+        this.time = time;
+        this.len = imgList.length;
+        this.id = `.${id}`;
+        this.current = 0;
+        slider.classList.add('slider');
+        slider.classList.add(id);
+        this.createSlides(imgList);
+        this.move();
+    }
+
+    createSlides(imgList){
+        imgList.forEach(ele => {
+            const container = document.createElement('div');
+            container.innerHTML = `<img src="${ele}" alt="">`;
+            container.classList.add('slide');
+            this.slider.appendChild(container);
+        });
+        const container = document.createElement('div');
+        container.innerHTML = `<img src="${imgList[0]}" alt="">`;
+        container.classList.add('slide');
+        this.slider.appendChild(container);
+        this.list = document.querySelectorAll(`${this.id} .slide`);
+    }
+
+    move(){
+        const imgSize = this.list[this.current].clientWidth;
+        if(this.current === this.len){
+            this.current = 0;
+            this.list.forEach(ele => ele.style.transition = "none");
+            this.list.forEach(ele => ele.style.transform = 'translateX(' + (-imgSize * this.current) + 'px)');
+            this.move();
+        }else{
+            ++this.current;
+            this.list.forEach(ele => ele.style.transition = "transform 0.7s ease-out");
+            this.list.forEach(ele => ele.style.transform = 'translateX(' + (-imgSize * this.current) + 'px)');
+            setTimeout(()=>this.move(), this.time)
+        }        
+    }
+}
