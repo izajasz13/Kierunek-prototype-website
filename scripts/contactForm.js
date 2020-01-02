bec92c0702eb88b1328d2f9edfde0a1f6c05f95c
izajasz13 <karoldd13@gmail.com>
2020-01-02 19:28:17 +0100

    Form submitions, form animations

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,18 +1,51 @@
-const onSubmit = (e) => {
+const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target.parentNode;
     const imieInput = document.querySelector(".imie");
     const emailInput = document.querySelector(".email");
     const wiadomoscInput = document.querySelector(".wiadomość");
-    const imie = imieInput.value;
+    const name = imieInput.value;
     const email = emailInput.value;
-    const wiadomosc = wiadomoscInput.value;
+    const message = wiadomoscInput.value;
 
-    console.log(imie);
-    console.log(email);
-    console.log(wiadomosc);
+    const obj = {
+        name,
+        email,
+        message
+    };
 
-    imieInput.value = '';
-    emailInput.value = '';
-    wiadomoscInput.value = '';
+    try {
+        const data = await postData('http://localhost:3000/contact', obj);
+        form.classList.add("submitForm");
+        form.addEventListener('animationend', e => {
+            e.currentTarget.classList.remove("submitForm")
+            imieInput.value = '';
+            emailInput.value = '';
+            wiadomoscInput.value = '';
+        });
+    }
+    catch (error) {
+        console.error(error, 'abc');
+    }
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    console.log(data);
+    return await response;
 }
 
 export const attachContactListeners = () => {
